65ec2a57a061bbb3a149a656e94530e15337f820
inbarshani <inbar.shani.il@gmail.com>
2020-11-16 21:14:07 +0200

    discount w/o

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,22 +21,36 @@ export class AppComponent {
   };
 
   sumPerHead(year: number): number {
+    const baseCost = 315;
     return Math.round(
-      this.adults * 314 +
-        (this.assisted * 314 * 25) / 100 +
-        (this.elderly * 314 * 75) / 100 +
-        (this.youngs * 314 * 0) / 100 +
-        ((this.hiring * this.hiringDiscounts["" + year]) / 100) * 314
+      this.adults * baseCost +
+        (this.assisted * baseCost * 25) / 100 +
+        (this.elderly * baseCost * 75) / 100 +
+        (this.youngs * baseCost * 0) / 100 +
+        ((this.hiring * this.hiringDiscounts["" + year]) / 100) * baseCost
     );
   }
   sumPerHeadAndArea(year: number): number {
+    const baseCost = 254;
     return Math.round(
-      this.adults * 257 +
-        (this.assisted * 257 * 0) / 100 +
-        (this.elderly * 257 * 50) / 100 +
-        (this.youngs * 257 * 0) / 100 +
-        (this.hasArea ? 128 : 0) +
-        ((this.hiring * this.hiringDiscounts["" + year]) / 100) * 257
+      this.adults * baseCost +
+        (this.assisted * baseCost * 0) / 100 +
+        (this.elderly * baseCost * 70) / 100 +
+        (this.youngs * baseCost * 0) / 100 +
+        (this.hasArea ? baseCost / 2 : 0) +
+        ((this.hiring * 100) / 100) * baseCost
+    );
+  }
+
+  sumPerHeadAndAreaWithDiscount(year: number): number {
+    const baseCost = 254;
+    return Math.round(
+      this.adults * baseCost +
+        (this.assisted * baseCost * 0) / 100 +
+        (this.elderly * baseCost * 70) / 100 +
+        (this.youngs * baseCost * 0) / 100 +
+        (this.hasArea ? baseCost / 2 : 0) +
+        ((this.hiring * this.hiringDiscounts["" + year]) / 100) * baseCost
     );
   }
 }
